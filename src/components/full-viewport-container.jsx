efb01e03c6e0d4c14e0a576dd67c4b8b3bba8c50
joubierjc <joubier.j.c@gmail.com>
2021-01-26 23:15:22 +0100

    progress :o 1/?
    added intro & work-in-progress components

--- src/components/full-viewport-container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './full-viewport-container.css';
-
-export function FullViewportContainer({children}) {
-
-    return <div className="full-viewport-container">
-        {children}
-    </div>
-}
